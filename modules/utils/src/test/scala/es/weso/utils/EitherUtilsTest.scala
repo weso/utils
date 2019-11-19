cc0361aec554503c8d9270a62018afc1164c8441
Jose Labra <jelabra@gmail.com>
2019-11-19 08:06:05 +0100

    Added test

--- modules/utils/src/test/scala/es/weso/utils/EitherUtilsTest.scala
+++ modules/utils/src/test/scala/es/weso/utils/EitherUtilsTest.scala
@@ -11,4 +11,13 @@ class EitherUtilsTest extends FunSpec with Matchers {
          els.fold(e => fail(s"Should have a value"), v => v.length should(be (2)))
      }
  }
+
+ describe(s"Take single") {
+     it(s"Should take from empty list") {
+         val msg = "failed"
+         val e:Either[String,Int] = takeSingle(List[Int](), msg)
+         e.fold(s => s should(be(msg)), _ => fail(s"Should have failed"))     
+     }
+ }
+
 }
